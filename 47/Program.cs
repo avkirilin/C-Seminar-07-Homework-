c4a287d2ede4b250bf53a9efaf219f78337bc967
Alexander Kirilin <nerosashok@mail.ru>
2023-01-27 14:50:39 +0300

    Сделано домашнее задание к 7 семинару C#

---
+++ 47/Program.cs
@@ -0,0 +1,40 @@
+﻿//Задайте двумерный массив размером m * n,
+//заполненный случайными вещественными числами
+
+double[,] GetArray(int rows, int columns, int minValue, int maxValue)                      //метод получения вещественного двумерного массива с рандомными значениями
+{
+    double[,] result = new double[rows, columns];
+    for (int i = 0; i < columns; i++)
+    {
+        for (int j = 0; j < rows; j++)
+        {
+            result[i, j] = Math.Round((new Random().Next(minValue, maxValue + 1) + new Random().NextDouble()), 3);
+        }
+    }
+    return result;
+}
+
+void PrintArray(double[,] inArray)                                                         //метод вывода двумерного массива с корректными отступами
+{
+    Console.WriteLine();
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            if (inArray[i, j] >= 0 && inArray[i, j] < 10) Console.Write($"    {inArray[i, j]:f3}");
+            if (inArray[i, j] >= 10 && inArray[i, j] < 100) Console.Write($"   {inArray[i, j]:f3}");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.WriteLine("Введите кол-во строк и кол-во столбцов массива через пробел");
+string[] f = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries)!;
+int rowsArray = int.Parse(f[0]);
+int columnsArray = int.Parse(f[1]);
+Console.WriteLine("Введите минимальное и максимальное значения элементов массива в диапазоне 0-99, через пробел");
+string[] d = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries)!;
+int minNum = int.Parse(d[0]);
+int maxNum = int.Parse(d[1]);
+double[,] myArray = GetArray(rowsArray, columnsArray, minNum, maxNum);
+PrintArray(myArray);
---
+++ 50/Program.cs
@@ -0,0 +1,54 @@
+﻿//Напишите программу, которая на вход принимает позиции элемента в двумерном массиве и возвращает
+//значение этого элемента или же указание, что такого элемента нет.
+int[,] GetArray(int rows, int columns, int minValue, int maxValue)                      //метод получения двумерного массива с рандомными значениями
+{
+    int[,] result = new int[rows, columns];
+    for (int i = 0; i < columns; i++)
+    {
+        for (int j = 0; j < rows; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+int FindItemInArray(int[,] array, int rows, int columns, int desiredNum)                //метод нахождения элемента по его порядковому номеру
+{
+    int searchingRow = desiredNum / columns;
+    int searchingColumn = desiredNum % columns;
+    int result = array[searchingRow, searchingColumn - 1];
+    return result;
+}
+
+void PrintArray(int[,] inArray)                                                         //метод вывода двумерного массива с корректными отступами
+{
+    Console.WriteLine();
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            if (inArray[i, j] >= 0 && inArray[i, j] < 10) Console.Write($"     {inArray[i, j]}");
+            if (inArray[i, j] >= 10 && inArray[i, j] < 100) Console.Write($"    {inArray[i, j]}");
+            if (inArray[i, j] >= 100 && inArray[i, j] < 1000) Console.Write($"   {inArray[i, j]}");
+            if (inArray[i, j] >= 1000 && inArray[i, j] < 10000) Console.Write($"  {inArray[i, j]}");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+Console.WriteLine("Введите кол-во строк и кол-во столбцов массива через пробел");
+string[] f = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+int rowsArray = int.Parse(f[0]);
+int columnsArray = int.Parse(f[1]);
+Console.WriteLine("Введите минимальное и максимальное значения элементов массива в диапазоне 0-9999, через пробел");
+string[] d = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+int minNum = int.Parse(d[0]);
+int maxNum = int.Parse(d[1]);
+Console.WriteLine("Введите порядковый номер искомого элемента в массиве");
+int indexNum = Convert.ToInt32(Console.ReadLine());
+int[,] myArray = GetArray(rowsArray, columnsArray, minNum, maxNum);
+PrintArray(myArray);
+if (rowsArray * columnsArray >= indexNum) Console.WriteLine($"Значение искомого элемента массива равно: {FindItemInArray(myArray, rowsArray, columnsArray, indexNum)}");
+else Console.WriteLine($"Искомое значение лежит за пределами заданного массива");
